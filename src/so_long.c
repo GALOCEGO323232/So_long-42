1aeec9ca1a3af50c0711ff2c0078cd9c0888c21e
Tester <dragaomatadordeporco@gmail.com>
2025-09-03 15:54:19 -0300

    Add files via upload

--- src/so_long.c
+++ src/so_long.c
@@ -1,29 +1,38 @@
 #include "so_long.h"
 
+static void	init_game(t_game *game, char *map_file)
+{
+	game->mlx = mlx_init();
+	if (!game->mlx)
+		error_exit("Erro ao iniciar MLX");
+	game->win = mlx_new_window(game->mlx, 1000, 1000, "so_long");
+	if (!game->win)
+		error_exit("Erro ao criar janela");
+	game->x = 0;
+	game->y = 0;
+	game->player_dir = DIR_DOWN;
+	game->player_frame = 0;
+	game->moves = 0;
+	game->c = 0;
+	game->collected = 0;
+	game->e = 0;
+	game->p = 0;
+	read_map(game, map_file);
+	file_to_image(game);
+	draw_map(game);
+}
+
 int	main(int argc, char **argv)
 {
 	t_game	game;
 
 	if (argc != 2)
 		error_exit("Uso: ./so_long <mapa.ber>");
-	game.mlx = mlx_init();
-	if (!game.mlx)
-		error_exit("Erro ao iniciar MLX");
-	game.win = mlx_new_window(game.mlx, 1000, 1000, "so_long");
-	if (!game.win)
-		error_exit("Erro ao criar janela");
-	game.x = 0;
-	game.y = 0;
-	game.player_dir = DIR_DOWN;
-	game.player_frame = 0;
-	game.moves = 0;
-	game.c = 0;
-	game.collected = 0;
-	game.e = 0;
-	game.p = 0;
-	read_map(&game, argv[1]);
-	file_to_image(&game);
-	draw_map(&game);
-
+	init_game(&game, argv[1]);
+	mlx_key_hook(game.win, handle_input, &game);
+	mlx_hook(game.win, 17, 0, close_game, &game);
+	mlx_loop(game.mlx);
+	return (0);
 }
+
 
